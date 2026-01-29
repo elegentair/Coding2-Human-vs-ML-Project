d3f5fcf7bf190cb5d50f670fa2f37866b8cf60e8
Sam <49598332+elegentair@users.noreply.github.com>
2026-01-29 21:05:46 +0000

    Making changes

--- data/get_data.py
+++ data/get_data.py
@@ -1,1 +1,16 @@
 #This will be for getting our data
+
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+spambase = fetch_ucirepo(id=94) 
+  
+# data (as pandas dataframes) 
+X = spambase.data.features 
+y = spambase.data.targets 
+  
+# metadata 
+print(spambase.metadata) 
+  
+# variable information 
+print(spambase.variables) 
