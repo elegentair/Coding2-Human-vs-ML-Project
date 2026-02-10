88f829078a98ba781fc9d75d81f972dec0269a46
Sam <49598332+elegentair@users.noreply.github.com>
2026-02-10 21:02:29 +0000

    Initial

--- data/get_data.py
+++ data/get_data.py
@@ -1,12 +1,14 @@
 #This will be for getting our data
 import pandas as pd
+import matplotlib.pyplot as plt
+# The following was copied from the uciml docs: https://archive.ics.uci.edu/dataset/94/spambase
 from ucimlrepo import fetch_ucirepo 
   
 # fetch dataset 
 spambase = fetch_ucirepo(id=94) 
   
 # data (as pandas dataframes) 
-X = spambase.data.features 
+x = spambase.data.features 
 y = spambase.data.targets 
   
 # metadata 
@@ -15,5 +17,11 @@ y = spambase.data.targets
 # variable information 
 #print(spambase.variables)
 
-spambase.head()
+
+#For "getting started" section of project:
+df = pd.concat([x, y], axis=1)
+
+plt.plot(df["word_freq_telnet"], df["word_freq_mail"])
+plt.savefig("data/plot1.png")
+
 
